07f51281221e22bfc195627402c835ed8dcff02f
Marcello Correia <marcellocorreia@gmail.com>
2025-09-13 10:41:08 -0300

    feat(tests): add feature tests for interview CRUD and submission review flows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
+/**
+ * Base TestCase for application tests.
+ */
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+    use RefreshDatabase;
 }
